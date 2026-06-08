19da10c52dc4e8ade063e4724a93ebd3ff5e1f57
Raj Posture <rajposture16@gmail.com>
2026-06-08 18:20:50 +0530

    db setup

---
+++ scripts/ingest_data.py
@@ -0,0 +1,25 @@
+import sys
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(ROOT_DIR))
+
+from backend.rag.embedder import generate_embedding
+from backend.rag.vector_store import collection
+
+file_path = Path("data/raw/news.txt")
+
+documents = file_path.read_text().split("\n")
+
+for idx, doc in enumerate(documents):
+
+    if not doc.strip():
+        continue
+
+    collection.add(
+        ids=[str(idx)],
+        documents=[doc],
+        embeddings=[generate_embedding(doc)]
+    )
+
+print("Data Ingested Successfully")
